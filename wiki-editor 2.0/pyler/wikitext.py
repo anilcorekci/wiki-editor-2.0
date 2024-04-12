1288957cdc301c51019ffbd247cfe6ffcd4d5a57
anilcorekci <35172975+anilcorekci@users.noreply.github.com>
2024-04-12 07:03:09 +0300

    Update wikitext.py

--- wiki-editor 2.0/pyler/wikitext.py
+++ wiki-editor 2.0/pyler/wikitext.py
@@ -10,25 +10,35 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk as gtk
 from gi.repository import Gdk as gdk
 from gi.repository import GtkSource as edit
+import os
 
-	
+ileti = "From main function hito -> ileti gtk.label"
+statu = "From main function hito -> statu gtk.label"
+
 class wikieditor(gtk.ScrolledWindow):
 	def __init__(self):
 		gtk.ScrolledWindow.__init__(self)
 	#	self.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
 		self.add(self.widget())
+
+		#### getting variables from __main__ hito
+		import __main__ as main_
+		globals()["ileti"] = main_.hito.ileti
+		globals()["statu"] =  main_.hito.statu
+		############################
+
+	def get_widget(self): return self.edit
+
 	def widget(self):
 		self.edit = edit.View()
 		self.tbuffer =  edit.Buffer()
 		self.edit.set_buffer(self.tbuffer)
 		self.edit.set_wrap_mode(gtk.WrapMode.CHAR)	
-	#	self.edit.set_border_window_size(gtk.TEXT_WINDOW_LEFT, 57)
-	#	self.edit.set_border_window_size(gtk.TEXT_WINDOW_RIGHT, 2)
+
 		self.tbuffer.connect('changed' , self.update_cursor_position,self.edit)
 		self.edit.set_size_request( 490, 70) 
 		return self.edit
-	def get_widget(self):
-		return self.edit
+	
 	def arama(self, w, data):
 		builder = gtk.Builder()        
 		builder.add_from_file("../Glade/ara.glade")	
@@ -36,11 +46,8 @@ class wikieditor(gtk.ScrolledWindow):
 		self.search_text.set_size_request(170 ,30)
 		self.search_text.connect("activate", self.devam)
 
-		 #have look this again ....
-		
-		self.log =  builder.get_object("radiobutton1")
-		self.log1 = builder.get_object("radiobutton2")
-		self.log2  = builder.get_object("radiobutton3")
+		self.radio = {i: builder.get_object("radiobutton"+ str(i)) for i in range(1,4)}
+
 
 		buton = gtk.Button(" Bul ")
 		buton.connect("clicked",self.devam) 
@@ -82,10 +89,9 @@ class wikieditor(gtk.ScrolledWindow):
 				match_start, match_end = bul
 				i += 1
 				buffer.apply_tag(self.renk, match_start, match_end)
-				iter = match_end			
-		if not str:
-			ileti.set_text("Hiçbir Eşleşme Bulunamadı!")			
-		elif i < 1:
+				iter = match_end
+		
+		if not str or i < 1:
 			ileti.set_text("Hiçbir Eşleşme Bulunamadı!")
 		else:
 			ileti.set_text("%d Tane Eşleşme Bulundu.." % i)
@@ -106,14 +112,11 @@ class wikieditor(gtk.ScrolledWindow):
 			ileti.set_text("Hiçbir Eşleşme Bulunamadı!")
 
 	def devam(self,  forward):
-		# this is where it should be changed...
-		if self.log.get_active():
-			self.ara(True)		
-		elif self.log1.get_active():
-			self.ara(False)	
-		elif self.log2.get_active():
-			self.heim(1)		
-
+		radio_action = { 1:["ara",True], 2: ["ara",False], 3: ["heim",1] }
+		for radio in self.radio:
+			if self.radio[radio].get_active():
+				action, value = radio_action[radio]
+				getattr(self, action)(value)
 
 	def get_lines(self, first_y, last_y, buffer_coords, numbers):
 		text_view = self.edit
@@ -166,8 +169,7 @@ class wikieditor(gtk.ScrolledWindow):
 		compositor.set_wrap_mode(gtk.WrapMode.CHAR)
 		compositor.set_highlight_syntax(True)
 		compositor.set_header_format(False, 'Yazılan Gün  %A', None, '%F')
-		filename = data
-		filename = str(filename)
+		filename = str(data)
 		compositor.set_footer_format(True, '%T', filename, 'Sayfa %N/%Q')
 		compositor.set_print_header(True)
 		compositor.set_print_footer(True)
@@ -180,7 +182,7 @@ class wikieditor(gtk.ScrolledWindow):
 		if res == gtk.PrintOperationResult.ERROR:
 			self.hata("Hatalı Baskı Dosyası:\n\n" + filename)
 		elif res == gtk.PrintOperationResult.APPLY:
-			print  ('Baskı Dosyası: "%s"' % filename )
+			print  ('Baskı Dosyası: "%s"' % os.path.basename(filename ))
 
 	def begin_print_cb(self,operation, context, compositor):
 		while not compositor.paginate(context):
